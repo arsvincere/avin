6000ababd3e52a5e8355193c7699e6b1f662afd7
H1ghSpeed <bovyrinnikita@gmail.com>
2025-07-15 19:01:02 +0300

    add index support

---
+++ avin_data/src/exceptions.py
@@ -0,0 +1,9 @@
+"""Custom exceptions."""
+
+
+class TickerNotFound(Exception):
+    """Ticker not found exception."""
+
+
+class NotImplemetedCategory(Exception):
+    """Category is not implemented."""
--- avin_data/src/manager/source.py
+++ avin_data/src/manager/source.py
@@ -10,18 +10,30 @@ from __future__ import annotations
 
 import enum
 
+from src.exceptions import NotImplemetedCategory
+
 
 class Source(enum.Enum):
+    """Stock exchange."""
     MOEX = 1
     TINKOFF = 2
 
     @classmethod
     def from_str(cls, string: str) -> Source:
-        sources = {
-            "MOEX": Source.MOEX,
-            "TINKOFF": Source.TINKOFF,
-        }
-        return sources[string.upper()]
+        """Get enum from str.
+
+        Args:
+            string: category name.
+
+        Returns:
+            Category Enum.
+
+        Raises:
+            NotImplemetedCategory if category not exists.
+        """
+        if attr := getattr(cls, string.upper(), None):
+            return attr
+        raise NotImplemetedCategory(f"Category not implemented. Choice from {cls.__members__.keys()}")
 
 
 if __name__ == "__main__":
