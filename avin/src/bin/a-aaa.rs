a16d0142a249b4109d8207818e799bee3c474e59
Alex Avin <mr.alexavin@gmail.com>
2025-07-28 16:26:01 +0300

    cargo descriptions

--- avin/src/bin/a-aaa.rs
+++ avin/src/bin/a-aaa.rs
@@ -1,32 +1,10 @@
+#![allow(dead_code)]
+#![allow(unused)]
+
 use avin_core::*;
 use avin_strategy::*;
 use avin_utils::*;
 
 fn main() {
-    let iid = Manager::find_iid("moex_share_sber").unwrap();
-    let begin = str_date_to_utc("2023-08-01");
-    let end = str_date_to_utc("2023-09-01");
-    let tf = TimeFrame::H1;
-
-    let mut simulator = Simulator::new(&iid, begin, end);
-    simulator.activate(tf);
-
-    simulator.next_bar();
-    simulator.next_bar();
-    let chart = simulator.asset().chart(tf).unwrap();
-    println!("{}", chart.now().unwrap());
-
-    simulator.step(120);
-    let chart = simulator.asset().chart(tf).unwrap();
-    println!("{}", chart.now().unwrap());
-
-    let chart = simulator.asset().chart(tf).unwrap();
-    let now_bar = chart.now().unwrap();
-    let expect_dt = str_dt_to_utc("2023-08-01 12:00:00");
-    let expect_ts = expect_dt.timestamp_nanos_opt().unwrap();
-    let expect_bar =
-        Bar::new(expect_ts, 267.89, 268.39, 267.83, 268.31, 245660);
-    println!("{now_bar}");
-    println!("{expect_bar}");
-    assert_eq!(*now_bar, expect_bar);
+    println!("Hi");
 }
