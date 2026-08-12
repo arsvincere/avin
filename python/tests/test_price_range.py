c95de850cd3b4762a10b693aba346684e626f0ca
Alex Avin <mr.alexavin@gmail.com>
2026-08-12 23:11:32 +0300

    pytest PriceRange

---
+++ python/tests/test_price_range.py
@@ -0,0 +1,17 @@
+import pytest
+from avin import PriceRange
+
+
+def test_price_range():
+    r = PriceRange(100.0, 110.0)
+
+    assert r.low == 100.0
+    assert r.high == 110.0
+    assert r.width() == 10.0
+    assert r.middle() == 105.0
+    assert 105.0 in r
+
+
+def test_invalid_price_range():
+    with pytest.raises(ValueError):
+        PriceRange(110.0, 100.0)
