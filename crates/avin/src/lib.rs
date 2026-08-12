13c685b5b1d63612d3e9620227fbc0592b18a7c1
Alex Avin <mr.alexavin@gmail.com>
2026-08-12 22:43:01 +0300

    reexport PriceRange

---
+++ crates/avin/src/lib.rs
@@ -0,0 +1,18 @@
+// ───────────────────────────────────────────────────────────────────────────
+// AVIN
+// Understand the market before trading it.
+//
+// https://avin.info
+// ───────────────────────────────────────────────────────────────────────────
+// CREATED: 2023.07.23 15:06
+
+//! # AVIN  -  Ars Vincere (лат. искусство побеждать)
+//! ```text
+//!                             Open source cross-platform trading system
+//!                                      __   _    _  ___  __   _
+//!                                     /__\   \  /    |   | \  |
+//!                                    |    |   \/    _|_  |  \_|
+//!
+//! ```
+
+pub use avin_model::PriceRange;
--- crates/avin_model/src/lib.rs
+++ crates/avin_model/src/lib.rs
@@ -1,3 +1,10 @@
+// ───────────────────────────────────────────────────────────────────────────
+// AVIN
+// Understand the market before trading it.
+//
+// https://avin.info
+// ───────────────────────────────────────────────────────────────────────────
+
 mod price_range;
 
 pub use price_range::PriceRange;
