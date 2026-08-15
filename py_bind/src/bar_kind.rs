afeb1cda297206a9fbaaad964aad57f5d07f6cff
Alex Avin <mr.alexavin@gmail.com>
2026-08-15 20:21:28 +0300

    BarKind inner

--- py_bind/src/bar_kind.rs
+++ py_bind/src/bar_kind.rs
@@ -2,47 +2,51 @@ use pyo3::prelude::*;
 
 use avin::BarKind;
 
-#[repr(i8)]
 #[pyclass(name = "BarKind", eq, from_py_object)]
 #[derive(Clone, Copy, PartialEq, Eq)]
-pub enum PyBarKind {
-    Bull = 1,
-    Doji = 0,
-    Bear = -1,
+pub struct PyBarKind {
+    inner: BarKind,
 }
+
 #[pymethods]
 impl PyBarKind {
+    #[classattr]
+    #[allow(non_upper_case_globals)]
+    const Bull: Self = Self {
+        inner: BarKind::Bull,
+    };
+
+    #[classattr]
+    #[allow(non_upper_case_globals)]
+    const Doji: Self = Self {
+        inner: BarKind::Doji,
+    };
+
+    #[classattr]
+    #[allow(non_upper_case_globals)]
+    const Bear: Self = Self {
+        inner: BarKind::Bear,
+    };
+
     #[getter]
-    fn name(&self) -> &'static str {
-        match self {
-            Self::Bull => "Bull",
-            Self::Doji => "Doji",
-            Self::Bear => "Bear",
-        }
+    fn name(&self) -> String {
+        self.inner.to_string()
     }
 
     #[getter]
     fn value(&self) -> i8 {
-        *self as i8
+        self.inner as i8
     }
 }
 
 impl From<BarKind> for PyBarKind {
-    fn from(value: BarKind) -> Self {
-        match value {
-            BarKind::Bull => Self::Bull,
-            BarKind::Doji => Self::Doji,
-            BarKind::Bear => Self::Bear,
-        }
+    fn from(inner: BarKind) -> Self {
+        Self { inner }
     }
 }
 
 impl From<PyBarKind> for BarKind {
     fn from(value: PyBarKind) -> Self {
-        match value {
-            PyBarKind::Bull => Self::Bull,
-            PyBarKind::Doji => Self::Doji,
-            PyBarKind::Bear => Self::Bear,
-        }
+        value.inner
     }
 }
