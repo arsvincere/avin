02284ac4494cf1a14a3f7fc03a7ed66a47e07c72
Alex Avin <mr.alexavin@gmail.com>
2026-08-15 18:11:13 +0300

    del test

--- py_bind/src/bar_kind.rs
+++ py_bind/src/bar_kind.rs
@@ -34,15 +34,3 @@ impl From<PyBarKind> for BarKind {
         }
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn display() {
-        assert_eq!(BarKind::Bull.to_string(), "Bull");
-        assert_eq!(BarKind::Doji.to_string(), "Doji");
-        assert_eq!(BarKind::Bear.to_string(), "Bear");
-    }
-}
