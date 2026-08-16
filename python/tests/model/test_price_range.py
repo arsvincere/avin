d2a864e3193286e4a4c043e34400028cfbd2d490
Alex Avin <mr.alexavin@gmail.com>
2026-08-16 14:56:49 +0300

    tests non finite price range

--- python/tests/model/test_price_range.py
+++ python/tests/model/test_price_range.py
@@ -19,6 +19,30 @@ def test_invalid_price_range():
         PriceRange(110.0, 100.0)
 
 
+def test_non_finite_price_range():
+    nan = float("nan")
+    inf = float("inf")
+    neg_inf = float("-inf")
+
+    with pytest.raises(ValueError):
+        PriceRange(nan, 100.0)
+
+    with pytest.raises(ValueError):
+        PriceRange(100.0, nan)
+
+    with pytest.raises(ValueError):
+        PriceRange(inf, 100.0)
+
+    with pytest.raises(ValueError):
+        PriceRange(100.0, inf)
+
+    with pytest.raises(ValueError):
+        PriceRange(neg_inf, 100.0)
+
+    with pytest.raises(ValueError):
+        PriceRange(100.0, neg_inf)
+
+
 def test_price_range_str():
     r = PriceRange(123.5, 234.5)
 
@@ -27,6 +51,7 @@ def test_price_range_str():
 
 def test_price_range_eq():
     assert PriceRange(100.0, 110.0) == PriceRange(100.0, 110.0)
+
     assert PriceRange(100.0, 110.0) != PriceRange(100.0, 120.0)
     assert PriceRange(100.0, 110.0) != None
     assert PriceRange(100.0, 110.0) != "foo"
