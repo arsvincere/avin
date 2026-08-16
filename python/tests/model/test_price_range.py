13e90f64d049620eaa0318f369dee42ee2df2842
Alex Avin <mr.alexavin@gmail.com>
2026-08-16 14:43:10 +0300

    tests PriceRange

--- python/tests/model/test_price_range.py
+++ python/tests/model/test_price_range.py
@@ -8,14 +8,21 @@ def test_price_range():
 
     assert r.low == 100.0
     assert r.high == 110.0
+
     assert r.width() == 10.0
     assert r.middle() == 105.0
-    assert 105.0 in r
+    assert r.contains(105.1)
 
 
 def test_invalid_price_range():
     with pytest.raises(ValueError):
         PriceRange(110.0, 100.0)
+
+
+def test_price_range_str():
+    r = PriceRange(123.5, 234.5)
+
+    assert str(r) == "[123.5, 234.5]"
 
 
 def test_price_range_eq():
@@ -24,3 +31,14 @@ def test_price_range_eq():
     assert PriceRange(100.0, 110.0) != None
     assert PriceRange(100.0, 110.0) != "foo"
     assert PriceRange(100.0, 110.0) != 123
+
+
+def test_price_range_contains():
+    r = PriceRange(100.0, 110.0)
+
+    assert 100.0 in r
+    assert 106.6 in r
+    assert 110.0 in r
+
+    assert 99.9 not in r
+    assert 110.1 not in r
