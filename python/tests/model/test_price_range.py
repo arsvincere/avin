0b53659d46a08bc11d46c15076f28a9181bc0490
Alex Avin <mr.alexavin@gmail.com>
2026-08-18 11:46:46 +0300

    header

--- python/tests/model/test_price_range.py
+++ python/tests/model/test_price_range.py
@@ -1,3 +1,10 @@
+# ────────────────────────────────────────────────────────────────────────────
+#  AVIN
+#  Understand the market before trading it.
+#
+#  https://avin.info
+# ────────────────────────────────────────────────────────────────────────────
+
 import pytest
 
 from avin import PriceRange
