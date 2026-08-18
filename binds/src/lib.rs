bc4f0eeb0367b876dd3a1831aeb8fbc1b4888955
Alex Avin <mr.alexavin@gmail.com>
2026-08-18 17:56:57 +0300

    bind TimeFrame

--- binds/src/lib.rs
+++ binds/src/lib.rs
@@ -9,14 +9,15 @@ mod model;
 
 use pyo3::prelude::*;
 
-use model::{PyBarDirection, PyExchange, PyPriceRange};
+use model::{PyBarDirection, PyExchange, PyPriceRange, PyTimeFrame};
 
 #[pymodule]
 #[pyo3(name = "_native")]
 fn avin_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<PyPriceRange>()?;
     m.add_class::<PyBarDirection>()?;
     m.add_class::<PyExchange>()?;
+    m.add_class::<PyPriceRange>()?;
+    m.add_class::<PyTimeFrame>()?;
 
     Ok(())
 }
--- binds/src/model/mod.rs
+++ binds/src/model/mod.rs
@@ -8,7 +8,9 @@
 mod bar_direction;
 mod exchange;
 mod price_range;
+mod timeframe;
 
 pub use bar_direction::PyBarDirection;
 pub use exchange::PyExchange;
 pub use price_range::PyPriceRange;
+pub use timeframe::PyTimeFrame;
