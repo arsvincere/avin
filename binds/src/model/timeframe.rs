9070ed333801cd937ae3d3f7e59d99349bad8341
Alex Avin <mr.alexavin@gmail.com>
2026-08-19 18:34:40 +0300

    bind TimeFrame tests

--- binds/src/model/timeframe.rs
+++ binds/src/model/timeframe.rs
@@ -125,3 +125,92 @@ impl PyTimeFrame {
         self.inner.end_frame_ts(ts)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::*;
+
+    #[test]
+    fn variants_delegation() {
+        let cases = [
+            (PyTimeFrame::S1.inner, TimeFrame::S1),
+            (PyTimeFrame::S5.inner, TimeFrame::S5),
+            (PyTimeFrame::S10.inner, TimeFrame::S10),
+            (PyTimeFrame::S15.inner, TimeFrame::S15),
+            (PyTimeFrame::M1.inner, TimeFrame::M1),
+            (PyTimeFrame::M5.inner, TimeFrame::M5),
+            (PyTimeFrame::M10.inner, TimeFrame::M10),
+            (PyTimeFrame::M15.inner, TimeFrame::M15),
+            (PyTimeFrame::H1.inner, TimeFrame::H1),
+            (PyTimeFrame::H4.inner, TimeFrame::H4),
+            (PyTimeFrame::Day.inner, TimeFrame::Day),
+            (PyTimeFrame::Week.inner, TimeFrame::Week),
+            (PyTimeFrame::Month.inner, TimeFrame::Month),
+        ];
+        assert_eq!(cases.len(), TimeFrame::all().len());
+
+        for (bind, domain) in cases {
+            assert_eq!(bind, domain);
+        }
+    }
+
+    #[test]
+    fn all_delegation() {
+        let bind_all = PyTimeFrame::all();
+        let domain_all = TimeFrame::all();
+        assert_eq!(bind_all.len(), domain_all.len());
+
+        for (bind, domain) in bind_all.iter().zip(domain_all) {
+            assert_eq!(bind.inner, *domain);
+        }
+    }
+
+    #[test]
+    fn from_str_delegation() {
+        let bind = PyTimeFrame::from_str("15M").unwrap();
+        let domain = TimeFrame::from_str("15M").unwrap();
+
+        assert_eq!(bind.inner, domain);
+    }
+
+    #[test]
+    fn display_delegation() {
+        let bind = PyTimeFrame {
+            inner: TimeFrame::M15,
+        };
+
+        assert_eq!(bind.display(), bind.inner.to_string());
+    }
+
+    #[test]
+    fn eq_delegation() {
+        let a = PyTimeFrame {
+            inner: TimeFrame::M15,
+        };
+        let b = PyTimeFrame {
+            inner: TimeFrame::M15,
+        };
+        let c = PyTimeFrame {
+            inner: TimeFrame::H1,
+        };
+
+        assert_eq!(a.eq(&b), a.inner == b.inner);
+        assert_eq!(a.eq(&c), a.inner == c.inner);
+    }
+
+    #[test]
+    fn instance_methods_delegation() {
+        let bind = PyTimeFrame {
+            inner: TimeFrame::M15,
+        };
+        assert_eq!(bind.nanos(), bind.inner.nanos());
+        assert_eq!(bind.seconds(), bind.inner.seconds());
+        assert_eq!(bind.timedelta(), bind.inner.timedelta());
+
+        let ts = 1_691_000_123_456_789_000;
+        assert_eq!(bind.begin_frame_ts(ts), bind.inner.begin_frame_ts(ts));
+        assert_eq!(bind.end_frame_ts(ts), bind.inner.end_frame_ts(ts));
+    }
+}
