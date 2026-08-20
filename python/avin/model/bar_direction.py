f8fd0e4840134806a1530acf2a10254662327172
Alex Avin <mr.alexavin@gmail.com>
2026-08-20 18:57:52 +0300

    py upd BarDirection

--- python/avin/model/bar_direction.py
+++ python/avin/model/bar_direction.py
@@ -20,32 +20,45 @@ class BarDirection(Enum):
 
     Attributes
     ----------
-    Bull
+    BULL
         The bar closes above its opening price.
-    Neutral
+    NEUTRAL
         The bar closes at its opening price.
-    Bear
+    BEAR
         The bar closes below its opening price.
 
     Examples
     --------
-    >>> direction = BarDirection.Bull
+    >>> direction = BarDirection.BULL
     >>> direction.name
-    'Bull'
+    'BULL'
     >>> direction.value
     1
     >>> str(direction)
     'Bull'
     """
 
-    # TODO: сделать ли значения PyBarDirection.Variant как в Exchange?
-    Bull = PyBarDirection.Bull.value()
-    Neutral = PyBarDirection.Neutral.value()
-    Bear = PyBarDirection.Bear.value()
+    BULL = PyBarDirection.Bull
+    NEUTRAL = PyBarDirection.Neutral
+    BEAR = PyBarDirection.Bear
+
+    _inner: PyBarDirection
+
+    def __new__(cls, inner: PyBarDirection):
+        obj = object.__new__(cls)
+        obj._value_ = inner.value()
+        obj._inner = inner
+        return obj
 
     def __str__(self) -> str:
-        return self.name
+        return self._inner.display()
 
     @classmethod
     def _from_native(cls, inner: PyBarDirection) -> BarDirection:
-        return cls(inner.value())
+        for direction in cls:
+            if direction._inner.eq(inner):
+                return direction
+
+        raise RuntimeError(
+            "native bar direction is missing from BarDirection"
+        )
--- python/tests/model/test_bar_direction.py
+++ python/tests/model/test_bar_direction.py
@@ -9,17 +9,10 @@ from avin import BarDirection
 
 
 def test_bar_direction():
-    assert BarDirection.Bull == BarDirection.Bull
-    assert BarDirection.Bull != BarDirection.Bear
+    assert str(BarDirection.BULL) == "Bull"
+    assert str(BarDirection.NEUTRAL) == "Neutral"
+    assert str(BarDirection.BEAR) == "Bear"
 
-    assert str(BarDirection.Bull) == "Bull"
-    assert str(BarDirection.Neutral) == "Neutral"
-    assert str(BarDirection.Bear) == "Bear"
-
-    assert BarDirection.Bull.name == "Bull"
-    assert BarDirection.Neutral.name == "Neutral"
-    assert BarDirection.Bear.name == "Bear"
-
-    assert BarDirection.Bull.value == 1
-    assert BarDirection.Neutral.value == 0
-    assert BarDirection.Bear.value == -1
+    assert BarDirection.BULL.value == 1
+    assert BarDirection.NEUTRAL.value == 0
+    assert BarDirection.BEAR.value == -1
