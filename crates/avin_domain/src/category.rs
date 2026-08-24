c638610e33e435b79204c6119c01c4d51e0e3c52
Alex Avin <mr.alexavin@gmail.com>
2026-08-24 11:55:56 +0300

    Category sort + todo

--- crates/avin_domain/src/category.rs
+++ crates/avin_domain/src/category.rs
@@ -10,15 +10,17 @@ use std::str::FromStr;
 
 use avin_utils::AvinError;
 
+// TODO: rename Currency -> CurrencyPair???
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Category {
-    Currency,
     Index,
     Share,
     Future,
     Bond,
     Option,
     ETF,
+    /// Currency pair / FX market instruments.
+    Currency,
 }
 
 /// Category.
@@ -44,13 +46,13 @@ impl Category {
     /// Returns all supported categories.
     pub const fn all() -> &'static [Self] {
         &[
-            Self::Currency,
             Self::Index,
             Self::Share,
             Self::Future,
             Self::Bond,
             Self::Option,
             Self::ETF,
+            Self::Currency,
         ]
     }
 }
@@ -58,13 +60,13 @@ impl Category {
 impl Display for Category {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Currency => f.write_str("CURRENCY"),
             Self::Index => f.write_str("INDEX"),
             Self::Share => f.write_str("SHARE"),
             Self::Future => f.write_str("FUTURE"),
             Self::Bond => f.write_str("BOND"),
             Self::Option => f.write_str("OPTION"),
             Self::ETF => f.write_str("ETF"),
+            Self::Currency => f.write_str("CURRENCY"),
         }
     }
 }
@@ -100,13 +102,13 @@ impl FromStr for Category {
     /// ```
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s.to_ascii_lowercase().as_str() {
-            "currency" => Ok(Self::Currency),
             "index" => Ok(Self::Index),
             "share" => Ok(Self::Share),
             "future" => Ok(Self::Future),
             "bond" => Ok(Self::Bond),
             "option" => Ok(Self::Option),
             "etf" => Ok(Self::ETF),
+            "currency" => Ok(Self::Currency),
             _ => {
                 let available = Self::all()
                     .iter()
@@ -132,13 +134,13 @@ mod tests {
     #[test]
     fn all() {
         let expected = [
-            Category::Currency,
             Category::Index,
             Category::Share,
             Category::Future,
             Category::Bond,
             Category::Option,
             Category::ETF,
+            Category::Currency,
         ];
 
         assert_eq!(Category::all(), expected);
@@ -146,27 +148,27 @@ mod tests {
 
     #[test]
     fn display() {
-        assert_eq!(Category::Currency.to_string(), "CURRENCY");
         assert_eq!(Category::Index.to_string(), "INDEX");
         assert_eq!(Category::Share.to_string(), "SHARE");
         assert_eq!(Category::Future.to_string(), "FUTURE");
         assert_eq!(Category::Bond.to_string(), "BOND");
         assert_eq!(Category::Option.to_string(), "OPTION");
         assert_eq!(Category::ETF.to_string(), "ETF");
+        assert_eq!(Category::Currency.to_string(), "CURRENCY");
     }
 
     #[test]
     fn from_str() {
-        assert_eq!(
-            Category::from_str("CURRENCY").unwrap(),
-            Category::Currency
-        );
         assert_eq!(Category::from_str("INDEX").unwrap(), Category::Index);
         assert_eq!(Category::from_str("SHARE").unwrap(), Category::Share);
         assert_eq!(Category::from_str("FUTURE").unwrap(), Category::Future);
         assert_eq!(Category::from_str("BoNd").unwrap(), Category::Bond);
         assert_eq!(Category::from_str("OPTION").unwrap(), Category::Option);
         assert_eq!(Category::from_str("etf").unwrap(), Category::ETF);
+        assert_eq!(
+            Category::from_str("CURRENCY").unwrap(),
+            Category::Currency
+        );
 
         assert!(Category::from_str("foo").is_err());
     }
