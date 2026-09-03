e62bba9c5968cf3642eb24320ea7f7492f30ba91
Alex Avin <mr.alexavin@gmail.com>
2026-09-03 15:12:03 +0300

    refactor: hide constants

--- crates/avin_utils/src/lib.rs
+++ crates/avin_utils/src/lib.rs
@@ -12,9 +12,9 @@ mod error;
 mod misc;
 
 pub use cmd::Cmd;
-pub use constant::{
-    DAY_BEGIN, DAY_END, ONE_DAY, ONE_HOUR, ONE_MINUTE, ONE_SECOND, ONE_WEEK,
-};
+// pub use constant::{
+//     DAY_BEGIN, DAY_END, ONE_DAY, ONE_HOUR, ONE_MINUTE, ONE_SECOND, ONE_WEEK,
+// };
 // pub use dt::{next_month_start, prev_month_start};
 pub use error::AvinError;
 // pub use misc::read_toml;
