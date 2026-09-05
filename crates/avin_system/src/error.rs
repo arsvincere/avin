3f04117e95f6f93a8699bdf1049fb874ef79b6e1
Alex Avin <mr.alexavin@gmail.com>
2026-09-05 16:47:08 +0300

    refactor: system error

--- crates/avin_system/src/error.rs
+++ crates/avin_system/src/error.rs
@@ -8,25 +8,29 @@
 use std::error::Error;
 use std::fmt::Display;
 
+use avin_core::CoreError;
+use avin_domain::DomainError;
+
 #[derive(Debug)]
 pub enum SystemError {
+    Core {
+        message: String,
+        source: CoreError,
+    },
+    Domain {
+        message: String,
+        source: DomainError,
+    },
+    Io {
+        message: String,
+        source: std::io::Error,
+    },
+
     Value(String),   // invalid value
     Parse(String),   // parse error
     Key(String),     // key missing
     Missing(String), // missing value
     Process(String),
-    Io {
-        message: String,
-        source: std::io::Error,
-    },
-    // Zip {
-    //     message: String,
-    //     source: zip::result::ZipError,
-    // },
-    // Polars {
-    //     message: String,
-    //     source: PolarsError,
-    // },
     InstrumentInfo {
         message: String,
         source: Box<SystemError>,
@@ -50,18 +54,16 @@ impl SystemError {
 impl Display for SystemError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Value(msg) => write!(f, "{msg}"),
-            Self::Parse(msg) => write!(f, "{msg}"),
-            Self::Key(msg) => write!(f, "{msg}"),
-            Self::Missing(msg) => write!(f, "{msg}"),
-            Self::Process(msg) => write!(f, "{msg}"),
-
+            Self::Core { message, .. } => write!(f, "{message}"),
+            Self::Domain { message, .. } => write!(f, "{message}"),
             Self::Io { message, .. } => write!(f, "{message}"),
-            // Self::Zip { message, .. } => write!(f, "{message}"),
-            // Self::Polars { message, .. } => write!(f, "{message}"),
-            Self::InstrumentInfo { message, .. } => {
-                write!(f, "{message}")
-            }
+
+            Self::Value(message) => write!(f, "{message}"),
+            Self::Parse(message) => write!(f, "{message}"),
+            Self::Key(message) => write!(f, "{message}"),
+            Self::Missing(message) => write!(f, "{message}"),
+            Self::Process(message) => write!(f, "{message}"),
+            Self::InstrumentInfo { message, .. } => write!(f, "{message}"),
         }
     }
 }
@@ -69,14 +71,15 @@ impl Display for SystemError {
 impl Error for SystemError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
+            Self::Core { source, .. } => Some(source),
+            Self::Domain { source, .. } => Some(source),
+            Self::Io { source, .. } => Some(source),
+
             Self::Value(_) => None,
             Self::Parse(_) => None,
             Self::Key(_) => None,
             Self::Missing(_) => None,
             Self::Process(_) => None,
-            Self::Io { source, .. } => Some(source),
-            // Self::Zip { source, .. } => Some(source),
-            // Self::Polars { source, .. } => Some(source),
             Self::InstrumentInfo { source, .. } => Some(source),
         }
     }
